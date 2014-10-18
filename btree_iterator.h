9680ecffd5d9c15197523024833ea92ca8ba7465
okamis <henningphan@hotmail.com>
2014-10-18 20:24:27 +1100

    changed btree_iterator to work

--- btree_iterator.h
+++ btree_iterator.h
@@ -19,13 +19,12 @@ class btree_iterator{
   btree_iterator& operator++();
   bool operator==(const btree_iterator& other);
   bool operator!=(const btree_iterator& other);
-  btree_iterator( btree<T>* pCur=nullptr, btree<T>* pPrev=nullptr);
+  btree_iterator( btree<T>* pCur=nullptr, const T* value=nullptr);
 //  btree_iterator( btree<T>* pCur, btree<T>* pPrev);
   private:
   // TODO is typename required here?
-  btree<T>* _pCur;
-  btree<T>* _pPrev;
-  T holder;
+  btree<T>* _pTree;
+  const T* _value;
 
 };
 
